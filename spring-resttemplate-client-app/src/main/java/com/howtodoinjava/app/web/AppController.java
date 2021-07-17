86ca7f677aab3d2864c19114891c835703722e01
lokeshgupta1981 <howtodoinjava@gmail.com>
2021-07-18 01:30:42 +0530

    Update AppController.java

--- spring-resttemplate-client-app/src/main/java/com/howtodoinjava/app/web/AppController.java
+++ spring-resttemplate-client-app/src/main/java/com/howtodoinjava/app/web/AppController.java
@@ -40,8 +40,7 @@ public class AppController {
 	}
 
 	@GetMapping("all-users_v1/{id}")
-	public ResponseEntity<User> getById_v1(@PathVariable
-	final long id) {
+	public ResponseEntity<User> getById_v1(@PathVariable final long id) {
 		Map<String, String> params = new HashMap<>();
 		params.put("id", "1");
 
@@ -57,12 +56,12 @@ public class AppController {
 	}
 
 	@GetMapping("all-users_v2/{id}")
-	public ResponseEntity<User> getById_v2(@PathVariable
-	final long id) {
+	public ResponseEntity<User> getById_v2(@PathVariable final long id) {
 		Map<String, String> params = new HashMap<>();
 		params.put("id", "1");
 
-		ResponseEntity<User> responseEntity = restTemplate.getForEntity(URI_USERS_ID, User.class, params);
+		ResponseEntity<User> responseEntity 
+			= restTemplate.getForEntity(URI_USERS_ID, User.class, params);
 		return responseEntity;
 	}
 
@@ -74,27 +73,27 @@ public class AppController {
 		headers.set("X-HEADER_NAME", "XYZ");
 
 		HttpEntity<String> entity = new HttpEntity<>(headers);
-		ResponseEntity<User[]> responseEntity = restTemplate.exchange("/users", HttpMethod.GET, entity, User[].class);
+		ResponseEntity<User[]> responseEntity = restTemplate
+				.exchange("/users", HttpMethod.GET, entity, User[].class);
 		return responseEntity;
 	}
 
 	@GetMapping("all-users_v3/{id}")
-	public ResponseEntity<User> getById_v3(@PathVariable
-	final long id) {
+	public ResponseEntity<User> getById_v3(@PathVariable final long id) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		headers.set("X-HEADER_NAME", "XYZ");
 
 		HttpEntity<String> entity = new HttpEntity<>(headers);
-		ResponseEntity<User> responseEntity = restTemplate.exchange("/users/" + id, HttpMethod.GET, entity, User.class);
+		ResponseEntity<User> responseEntity = restTemplate
+				.exchange("/users/" + id, HttpMethod.GET, entity, User.class);
 		return responseEntity;
 	}
 
 	// ************POST APIs**************//
 
 	@PostMapping("users")
-	public User create(@RequestBody
-	final User newUser) {
+	public User create(@RequestBody final User newUser) {
 		User createdUser = restTemplate.postForObject(URI_USERS, newUser, User.class);
 		return createdUser;
 	}
