683d8f51f5734f89fba8ef331e21ff8cab69319c
lokeshgupta1981 <howtodoinjava@gmail.com>
2021-07-17 17:00:17 +0530

    Update UserController.java

--- spring-demo-webapp/src/main/java/com/howtodoinjava/app/controller/UserController.java
+++ spring-demo-webapp/src/main/java/com/howtodoinjava/app/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,7 +41,9 @@ public class UserController {
 		}
 	}
 
-	@PostMapping("users")
+	@PostMapping(path = "users", 
+			consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> create(@RequestBody User newUser) {
 		User user = userService.save(newUser);
 		if (user == null) {
